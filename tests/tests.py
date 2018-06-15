c593cda312f23750a11b237db6cac2e70b656c6a
Bruno R. Santos <bruesmanbruesman@hotmail.com>
2018-06-15 17:06:59 -0300

    added new tests for single function

--- tests/tests.py
+++ tests/tests.py
@@ -2,7 +2,7 @@ import unittest
 from diskspace import diskspace
 
 
-class MainTests(unittest.TestCase):
+class bytes_to_readable_tests(unittest.TestCase):
 
     def test_blocks_0(self):
         blocks = 0
@@ -13,6 +13,41 @@ class MainTests(unittest.TestCase):
         blocks = 1
         bytes_size = diskspace.bytes_to_readable(blocks)
         self.assertEqual('512.00B', bytes_size)
+
+    def test_return_type(self):
+        blocks = 0
+        bytes_size = diskspace.bytes_to_readable(blocks)
+        self.assertIsInstance(bytes_size, str)
+
+    def test_labels_0(self):
+        blocks = 10000000000
+        bytes_size = diskspace.bytes_to_readable(blocks)
+        label = bytes_size[-2:]
+        self.assertEqual('Tb', label)
+
+    def test_labels_1(self):
+        blocks = 10000000
+        bytes_size = diskspace.bytes_to_readable(blocks)
+        label = bytes_size[-2:]
+        self.assertEqual('Gb', label)
+
+    def test_labels_2(self):
+        blocks = 100000
+        bytes_size = diskspace.bytes_to_readable(blocks)
+        label = bytes_size[-2:]
+        self.assertEqual('Mb', label)
+
+    def test_labels_3(self):
+        blocks = 1000
+        bytes_size = diskspace.bytes_to_readable(blocks)
+        label = bytes_size[-2:]
+        self.assertEqual('Kb', label)
+
+    def test_labels_3(self):
+        blocks = 1
+        bytes_size = diskspace.bytes_to_readable(blocks)
+        label = bytes_size[-1:]
+        self.assertEqual('B', label)
 
 
 if __name__ == '__main__':
